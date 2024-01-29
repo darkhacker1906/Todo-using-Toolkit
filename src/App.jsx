0d04dc405f401faf117385f4947514c2d60b7287
darkhacker1906 <singhabhinandan634@gmail.com>
2024-01-29 17:04:15 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeDashBoard from './pages/HomeDashBoard'
+
+function App() {
+  return (
+    <div>
+      <HomeDashBoard/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/HomeDashBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeDashBoard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeDashBoard
